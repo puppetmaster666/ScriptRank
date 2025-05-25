1a5ead116db0ddd0c72fbdab088d3a480c052370
puppetmaster666 <iamtrashbhitch@gmail.com>
2025-05-25 21:28:24 +0200

    Create  _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,7 @@
+// pages/_app.tsx
+import type { AppProps } from 'next/app';
+import '@/styles/globals.css'; // Make sure this matches your folder structure
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
