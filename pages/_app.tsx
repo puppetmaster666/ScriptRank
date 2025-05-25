091f92110a3bf0c2f723a91d3f50b00f432cee3f
puppetmaster666 <iamtrashbhitch@gmail.com>
2025-05-25 21:32:03 +0200

    Add _app.tsx for Tailwind CSS support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // pages/_app.tsx
 import type { AppProps } from 'next/app';
 import '../styles/globals.css'; // Make sure this matches your folder structure
@@ -5,3 +6,11 @@ import '../styles/globals.css'; // Make sure this matches your folder structure
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+=======
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+>>>>>>> d47e51b (Add _app.tsx for Tailwind CSS support)
