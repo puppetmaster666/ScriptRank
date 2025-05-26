5276b6043899843792c73b85d8564274e14c15f3
puppetmaster666 <iamtrashbhitch@gmail.com>
2025-05-26 20:46:41 +0200

    Fix import path casing to match header.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
-import Header from '../components/Header';
+import Header from '../components/header';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
