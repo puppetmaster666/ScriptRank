e2e2785eafd33f99d417bace4597c30c46531e5d
puppetmaster666 <iamtrashbhitch@gmail.com>
2025-05-28 15:03:11 +0200

    Fix .gitignore and untrack node_modules

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import Header from '../components/header';
+import { Analytics } from '@vercel/analytics/react'; // ✅ Import
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
       <Component {...pageProps} />
+      <Analytics /> {/* ✅ Add here */}
     </>
   );
 }
