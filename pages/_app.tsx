69966a116deff4dba5d466d5eb18a3954837f7c0
puppetmaster666 <iamtrashbhitch@gmail.com>
2025-08-24 21:25:01 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import Header from '../components/header';
 import { Analytics } from '@vercel/analytics/react'; // ✅ Import
+import '../styles/fonts.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
