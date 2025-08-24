d43e1de839a3d1f4257211d903011fe382c3050b
puppetmaster666 <iamtrashbhitch@gmail.com>
2025-08-25 00:36:52 +0200

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -160,10 +160,7 @@ export default function Header() {
                       onClick={() => setShowNotifications(!showNotifications)}
                       className="nav-link relative p-2"
                     >
-                      <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                        <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />
-                        <path d="M13.73 21a2 2 0 0 1-3.46 0" />
-                      </svg>
+                      <span className="text-xl">🔔</span>
                       {unreadCount > 0 && (
                         <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center font-bold">
                           {unreadCount}
