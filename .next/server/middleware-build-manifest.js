3cffab32e4600970c8a23a3fbc25870f7d429224
puppetmaster666 <iamtrashbhitch@gmail.com>
2025-08-25 05:40:28 +0200

    k

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -24,11 +24,6 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
-    ],
-    "/ideas/[id]": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/ideas/[id].js"
     ]
   },
   "ampFirstPages": []
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -181,7 +181,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("f47a08393b60dc94")
+/******/ 		__webpack_require__.h = () => ("ef9431a79a124e45")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
